3fa6450e17c59b845369a7a5531b220bc4b632a4
HongXi <slsssyb@gmail.com>
2022-02-15 19:38:43 -0500

    forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c678d11a-430d-45db-aa0e-aab48e2f3af8';
+export const MODAL_CLOSE_SEC = 2.5;
